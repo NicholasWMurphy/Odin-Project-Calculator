d736d41e22b80be47c3b181951b50aca5649297a
NicholasWMurphy <nickmurphy68@icloud.com>
2025-04-15 19:04:43 +1000

    created the operate function that takes a callback function for the operator

--- script.js
+++ script.js
@@ -16,3 +16,15 @@ const divide = function(a, b) {
     return a / b;
 }
 
+//creating variables to update calculator display
+var firstNumber;
+var secondNumber;
+var operator;
+
+//function that takes two numbers and a callback function as the operator
+
+function operate (operator, a, b) {
+    return operator(a, b);
+}
+
+console.log(operate(subtract, 5, 5))
