b28f2b633c06fce615b4c3d7051363f7b306a7e1
NicholasWMurphy <nickmurphy68@icloud.com>
2025-05-07 12:07:40 +1000

    test

--- script.js
+++ script.js
@@ -21,6 +21,7 @@ var firstNumber;
 var secondNumber;
 var operator;
 
+
 //function that takes two numbers and a callback function as the operator
 
 function operate (operator, a, b) {
@@ -29,19 +30,51 @@ function operate (operator, a, b) {
 
 console.log(operate(subtract, 5, 5))
 
-const btn = document.querySelectorAll("button");
-const display = document.querySelector("#display-numbers");
+const btn = document.querySelectorAll(".button");
+const displayNumbers = document.querySelector("#display-numbers");
+const operatorButton = document.querySelectorAll(".operator");
+const equals = document.querySelector(".equals");
+const clearButton = document.querySelector(".clear");
+const defaultZero = document.querySelector("#defaultZero");
 
-window.onload = function() {
-    display.textContent = '0';
-}
+var operatorButtonClicked = false;
+console.log(operatorButtonClicked)
+
+
 
 //pressing a button to update the display
 
 btn.forEach((button) => {
-    button.addEventListener("click", () => {
-        display.textContent = button.textContent;
-    })
+    button.addEventListener("click", (e) => {
+    defaultZero.textContent = '';
+    if (operatorButtonClicked === false) {
+        displayNumbers.textContent += button.id;
+        firstNumber = Number(displayNumbers.textContent);
+        console.log(firstNumber)
+    } 
+    }) 
 })
 
+
+
+//clear button that removes displayed numbers and brings back the default Zero;
+clearButton.addEventListener("click", () => {
+    displayNumbers.textContent = '';
+    defaultZero.textContent = '0';
+    operatorButtonClicked = false;
+}) 
+
+//operator button clicked check
+    operatorButton.forEach((button) => {
+    button.addEventListener("click", () => {
+    operatorButtonClicked = true;
+    operator = button.id;
+    let arr = []
+    arr.push(firstNumber + operator);
+    console.log(arr)
+    
+    })
+    })
+
+//retrieve 2nd number 
 
