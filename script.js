c178db514ad9e2c6559d687e1aebc8356d6a62b4
NicholasWMurphy <nickmurphy68@icloud.com>
2025-05-07 15:39:34 +1000

    stored the first and 2nd numbers surrounding an operator in an array

--- script.js
+++ script.js
@@ -20,6 +20,9 @@ const divide = function(a, b) {
 var firstNumber;
 var secondNumber;
 var operator;
+var arr;
+var test;
+
 
 
 //function that takes two numbers and a callback function as the operator
@@ -28,12 +31,12 @@ function operate (operator, a, b) {
     return operator(a, b);
 }
 
-console.log(operate(subtract, 5, 5))
+console.log(operate(subtract, 5, 4))
 
 const btn = document.querySelectorAll(".button");
 const displayNumbers = document.querySelector("#display-numbers");
 const operatorButton = document.querySelectorAll(".operator");
-const equals = document.querySelector(".equals");
+const equals = document.querySelectorAll(".equals");
 const clearButton = document.querySelector(".clear");
 const defaultZero = document.querySelector("#defaultZero");
 
@@ -48,7 +51,7 @@ btn.forEach((button) => {
     button.addEventListener("click", (e) => {
     defaultZero.textContent = '';
     if (operatorButtonClicked === false) {
-        displayNumbers.textContent += button.id;
+        displayNumbers.textContent += button.textContent;
         firstNumber = Number(displayNumbers.textContent);
         console.log(firstNumber)
     } 
@@ -62,19 +65,41 @@ clearButton.addEventListener("click", () => {
     displayNumbers.textContent = '';
     defaultZero.textContent = '0';
     operatorButtonClicked = false;
+    console.log(operatorButtonClicked)
+    arr = [];
+    
 }) 
 
 //operator button clicked check
     operatorButton.forEach((button) => {
     button.addEventListener("click", () => {
     operatorButtonClicked = true;
-    operator = button.id;
-    let arr = []
-    arr.push(firstNumber + operator);
-    console.log(arr)
-    
+    console.log(operatorButtonClicked)
+    operator = button.textContent;
+    test = button.id;
+    displayNumbers.textContent = firstNumber + operator;
+
     })
     })
 
-//retrieve 2nd number 
+//retrieve 2nd number and puts the first and 2nd number into an array.
 
+    btn.forEach((button) => {
+        button.addEventListener("click", (e) => {
+        if (operatorButtonClicked === true) {
+             displayNumbers.textContent += button.textContent;
+             secondNumber = displayNumbers.textContent;
+             arr = secondNumber.split(operator).map(Number);
+             console.log(arr);
+        } 
+        }) 
+    })
+
+//when equals is pressed, it runs the previously made operate function
+
+ equals.forEach((button) => {
+    button.addEventListener("click", () => {
+        console.log(operate(test, arr[0], arr[1]))
+
+    })
+    })
